9e637c0330524d5b57377a412a65998761218ea9
SandyFit <sandy07r@gmail.com>
2025-12-20 12:16:32 -0500

    style: apply Prettier formatting and ESLint fixes

--- api-tests/tests/booking.assertions.js
+++ api-tests/tests/booking.assertions.js
@@ -2,98 +2,92 @@ const { expect } = require("chai");
 const config = require("../config/config");
 
 const assertResponse = {
-    statusCode: (response, expectedStatus) => {
-        expect(response.status).to.equal(expectedStatus);
-    },
+  statusCode: (response, expectedStatus) => {
+    expect(response.status).to.equal(expectedStatus);
+  },
 
+  responseTime: (response, threshold = "medium") => {
+    expect(response.duration).to.be.below(
+      config.expectedResponseTime[threshold],
+    );
+  },
 
-    responseTime: (response, threshold = 'medium') => {
-        expect(response.duration).to.be.below(config.expectedResponseTime[threshold]);
-    },
+  jsonContentType: (response) => {
+    expect(response.headers).to.have.property("content-type");
+    expect(response.headers["content-type"]).to.include("application/json");
+  },
 
+  serverHeader: (response) => {
+    expect(response.headers).to.have.property("server");
+    expect(response.headers["server"]).to.exist;
+  },
 
-    jsonContentType: (response) => {
-        expect(response.headers).to.have.property("content-type");
-        expect(response.headers["content-type"]).to.include("application/json");
-    },
+  arrayBody: (response) => {
+    expect(response.data).to.be.an("array");
+    expect(response.data.length).to.be.greaterThan(0);
+  },
 
+  bookingIdInBody: (response) => {
+    expect(response.data).to.have.property("bookingid");
+    expect(response.data.bookingid).to.be.a("number");
+  },
 
-    serverHeader: (response) => {
-        expect(response.headers).to.have.property("server");
-        expect(response.headers["server"]).to.exist;
-    },
+  bookingPropertiesInBody: (responseData) => {
+    expect(responseData).to.have.property("firstname");
+    expect(responseData).to.have.property("lastname");
+    expect(responseData).to.have.property("totalprice");
+  },
 
-
-    arrayBody: (response) => {
-        expect(response.data).to.be.an("array");
-        expect(response.data.length).to.be.greaterThan(0);
-    },
-
-
-    bookingIdInBody: (response) => {
-        expect(response.data).to.have.property("bookingid");
-        expect(response.data.bookingid).to.be.a("number");
-    },
-
-
-    bookingPropertiesInBody: (responseData) => {
-        expect(responseData).to.have.property("firstname");
-        expect(responseData).to.have.property("lastname");
-        expect(responseData).to.have.property("totalprice");
-    },
-
-
-    bookingDataMatches: (responseData, expectedData) => {
-        if (expectedData.firstname) {
-            expect(responseData.firstname).to.equal(expectedData.firstname);
-        }
-        if (expectedData.lastname) {
-            expect(responseData.lastname).to.equal(expectedData.lastname);
-        }
-        if (expectedData.totalprice !== undefined) {
-            expect(responseData.totalprice).to.equal(expectedData.totalprice);
-        }
-        if (expectedData.depositpaid !== undefined) {
-            expect(responseData.depositpaid).to.equal(expectedData.depositpaid);
-        }
-        if (expectedData.bookingdates) {
-            if (expectedData.bookingdates.checkin) {
-                expect(responseData.bookingdates.checkin).to.equal(expectedData.bookingdates.checkin);
-            }
-            if (expectedData.bookingdates.checkout) {
-                expect(responseData.bookingdates.checkout).to.equal(expectedData.bookingdates.checkout);
-            }
-        }
-    },
-
-
-    authTokenInBody: (response) => {
-        expect(response.data).to.have.property("token");
-        expect(response.data.token).to.be.a("string");
-        expect(response.data.token.length).to.be.greaterThan(0);
-    },
-
-
-    schema: (response, schema) => {
-        const { error } = schema.validate(response.data);
-        expect(error).to.be.undefined;
-    },
-
-
-    errorStatusCode: (error, expectedStatus) => {
-        expect(error.status).to.equal(expectedStatus);
-    },
-
-
-    errorMessage: (error, expectedStatus) => {
-        expect(error.message).to.include(expectedStatus.toString());
-    },
-
-
-    errorStructure: (error) => {
-        expect(error).to.have.property("status");
-        expect(error.status).to.be.a("number");
+  bookingDataMatches: (responseData, expectedData) => {
+    if (expectedData.firstname) {
+      expect(responseData.firstname).to.equal(expectedData.firstname);
     }
+    if (expectedData.lastname) {
+      expect(responseData.lastname).to.equal(expectedData.lastname);
+    }
+    if (expectedData.totalprice !== undefined) {
+      expect(responseData.totalprice).to.equal(expectedData.totalprice);
+    }
+    if (expectedData.depositpaid !== undefined) {
+      expect(responseData.depositpaid).to.equal(expectedData.depositpaid);
+    }
+    if (expectedData.bookingdates) {
+      if (expectedData.bookingdates.checkin) {
+        expect(responseData.bookingdates.checkin).to.equal(
+          expectedData.bookingdates.checkin,
+        );
+      }
+      if (expectedData.bookingdates.checkout) {
+        expect(responseData.bookingdates.checkout).to.equal(
+          expectedData.bookingdates.checkout,
+        );
+      }
+    }
+  },
+
+  authTokenInBody: (response) => {
+    expect(response.data).to.have.property("token");
+    expect(response.data.token).to.be.a("string");
+    expect(response.data.token.length).to.be.greaterThan(0);
+  },
+
+  schema: (response, schema) => {
+    const { error } = schema.validate(response.data);
+    expect(error).to.be.undefined;
+  },
+
+  errorStatusCode: (error, expectedStatus) => {
+    expect(error.status).to.equal(expectedStatus);
+  },
+
+  errorMessage: (error, expectedStatus) => {
+    expect(error.message).to.include(expectedStatus.toString());
+  },
+
+  errorStructure: (error) => {
+    expect(error).to.have.property("status");
+    expect(error.status).to.be.a("number");
+  },
 };
 
 module.exports = { assertResponse };
